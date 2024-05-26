6461b82b73487fba24492e623bb83bfdfccb33b9
ubermensch <155133944+AlpianPPLG@users.noreply.github.com>
2024-05-26 19:03:14 +0800

    Little Update

--- src/App.js
+++ src/App.js
@@ -13,11 +13,21 @@ function App() {
 			return; 
 		} 
 		Axios.get( 
-`https://api.lyrics.ovh/v1/${artist}/${song}`).then(res => { 
-			console.log(res.data.lyrics); 
+	`https://api.lyrics.ovh/v1/${artist}/${song}`).then(res => { 
+		if (res.data.lyrics === null || res.data.lyrics === undefined || res.data.lyrics === "") { 
+			alert("Lagu tidak ditemukan! Mungkin ada typo atau judul lagu yang salah"); 
+		} else { 
 			setLyrics(res.data.lyrics); 
-		}) 
-	} 
+		}	 
+	})
+	.catch(err => {
+		if (err.response?.status === 404) {
+			alert("Lagu Dan Penyanyi Tidak Ditemukan!"); 
+		} else {
+			console.error(err);
+		}
+	}) 
+} 
 
 	return ( 
 		<div className="App"> 
